42e2f9aac39bb26b8343292e1c7f2e0dd8a57a10
thodorisKall <105176440+thodorisKall@users.noreply.github.com>
2025-02-04 02:13:01 +0200

    docs: delete starting code and add test code for requests apis

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { onMounted } from "vue"
+import { getDailyWeather, getCoordinates } from "./services/services"
+
+onMounted(async () => {
+  const weatherData = await getDailyWeather(50.9775106, 11.3285424)
+  console.log("Weather Data:", weatherData)
+
+  const coordinates = await getCoordinates("New York")
+  console.log("Coordinates:", coordinates)
+})
+</script>
+
+<template>
+  <h1>Weather App</h1>
+</template>
