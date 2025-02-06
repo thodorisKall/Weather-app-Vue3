3a2ff782ee04bb5a4f8fce928861c904de527d99
thodorisKall <105176440+thodorisKall@users.noreply.github.com>
2025-02-07 01:19:17 +0200

    feat: add function to display weather icon based on condition add more divs for better css alignment

--- src/App.vue
+++ src/App.vue
@@ -15,6 +15,31 @@ let todayWeather = ref<currentWeather | null>(null)
 let moon = ref<moonType | null>(null)
 let isLoading = ref(false)
 let errorMessage = ref<string | null>(null)
+import { MapPin, Search } from "lucide-vue-next"
+
+const weatherIcons: Record<string, string> = {
+  Clear: "☀️",
+  Clouds: "☁️",
+  Rain: "🌧️",
+  Drizzle: "🌦️",
+  Thunderstorm: "⛈️",
+  Snow: "❄️",
+  Mist: "🌫️",
+  Fog: "🌁",
+  Tornado: "🌪️",
+  Default: "🌈",
+}
+
+const getWeatherIcon = (description: string | undefined): string => {
+  if (!description) return weatherIcons.Default
+
+  const formattedDescription = description
+    .split(" ")
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(" ")
+
+  return weatherIcons[formattedDescription] || weatherIcons.Default
+}
 
 const handleSearch = async (): Promise<void> => {
   isLoading.value = true
@@ -31,12 +56,6 @@ const handleSearch = async (): Promise<void> => {
     moon.value = await getMoonData(lat, lng)
 
     todayWeather.value = await getDailyWeather(lat, lng)
-
-    console.log("Weather data:", todayWeather.value?.coord?.lon)
-    console.log(
-      "Moon data:",
-      moon.value?.moon_phases?.full_moon?.next?.datestamp
-    )
   } catch (error) {
     console.error("Error fetching data:", error)
     errorMessage.value = "Failed to fetch data. Please try again."
@@ -52,124 +71,116 @@ onMounted(() => {
 
 <template>
   <div v-if="coordinates && todayWeather && moon">
-    <section>
-      <div>
-        <h2>- {{ inputCity }}</h2>
-      </div>
-      <div>
+    <section class="weather_search">
+      <h2><MapPin /></h2>
+      <div class="weather_search_input">
         <input
           type="text"
           v-model="inputCity"
           placeholder="Press Enter City Name"
           @keydown.enter="handleSearch"
         />
+        <button @click="handleSearch"><Search /></button>
       </div>
-      <button @click="handleSearch">Search</button>
     </section>
-
-    <h1>Weather App</h1>
-    <div class="weather_basic">
-      <span></span>
-      <div>
-        <div>
-          <h3>
-            {{ coordinates?.results?.[0]?.formatted ?? "Location not found" }}
-            {{ coordinates?.results?.[0]?.annotations?.flag ?? "" }}
-          </h3>
-          <p>{{ todayWeather?.weather?.[0]?.description ?? "No data" }}</p>
+    <div class="weather_view">
+      <section class="weather_basic">
+        <span>{{ getWeatherIcon(todayWeather?.weather?.[0]?.main) }}</span>
+        <div class="weather_basic_today">
+          <div>
+            <h3>
+              {{ coordinates?.results?.[0]?.formatted ?? "Location not found" }}
+              {{ coordinates?.results?.[0]?.annotations?.flag ?? "" }}
+            </h3>
+            <p>{{ todayWeather?.weather?.[0]?.description ?? "No data" }}</p>
+          </div>
+          <h2>{{ todayWeather?.main?.temp.toFixed(1) ?? "N/A" }}&deg;</h2>
         </div>
-        <h2>{{ todayWeather?.main?.temp ?? "N/A" }}&deg;</h2>
-      </div>
-    </div>
-    <div v-if="todayWeather" class="weather_details">
-      <h3>Today's Highlights</h3>
-      <div>
-        <span></span>
-        <h4>Feels Like</h4>
-        <h5>{{ Math.round(todayWeather?.main?.feels_like ?? 0) }}&deg;</h5>
-      </div>
-      <div>
-        <span></span>
-        <h4>Min - Max Temp</h4>
-        <h5>
-          {{ Math.round(todayWeather?.main?.temp_min ?? 0) }}&deg; -
-          {{ Math.round(todayWeather?.main?.temp_max ?? 0) }}&deg;
-        </h5>
-      </div>
-      <div>
-        <span></span>
-        <h4>Visibility</h4>
-        <h5>{{ todayWeather?.visibility ?? "N/A" }} meters</h5>
-      </div>
-      <div>
-        <span></span>
-        <h4>Humidity</h4>
-        <h5>{{ todayWeather?.main?.humidity ?? "N/A" }} %</h5>
-      </div>
-      <div>
-        <span></span>
-        <h4>Pressure</h4>
-        <h5>{{ todayWeather?.main?.pressure ?? "N/A" }} hPa</h5>
-      </div>
-      <div>
-        <span></span>
-        <h4>Wind</h4>
-        <h5>{{ Math.trunc((todayWeather?.wind?.speed ?? 0) * 3.6) }} km/h</h5>
-      </div>
-    </div>
-    <aside v-if="moon" class="weather_moon">
-      <div>
-        <h4>Sunrise & Sunset:</h4>
-        <p>
-          {{ moon.sun?.sunrise_timestamp ?? "N/A" }}🌅-{{
-            moon.sun?.sunset_timestamp ?? "N/A"
-          }}🌇
-        </p>
-      </div>
-      <div>
-        <h4>Moonrise & Moonset:</h4>
-        <p>
-          {{ moon.moon?.moonrise ?? "N/A" }}🌙-{{
-            moon.moon?.moonset ?? "N/A"
-          }}🌓
-        </p>
-      </div>
-      <div>
-        <h4>Current Moon Phase:</h4>
-        <p>{{ moon.moon?.phase_name ?? "N/A" }}{{ moon.moon?.emoji ?? "" }}</p>
-      </div>
+      </section>
+      <section v-if="todayWeather" class="weather_details">
+        <div class="weather_details_box">
+          <img src="/img/feels_like.png" alt="thermometer icon" />
+          <h4>Feels Like</h4>
+          <h5>{{ Math.round(todayWeather?.main?.feels_like ?? 0) }}&deg;</h5>
+        </div>
+        <div class="weather_details_box">
+          <img src="/img/min_max.png" alt="meter icon" />
+          <h4>
+            Min - Max <br />
+            Temp
+          </h4>
+          <h5>
+            {{ Math.round(todayWeather?.main?.temp_min ?? 0) }}&deg; -
+            {{ Math.round(todayWeather?.main?.temp_max ?? 0) }}&deg;
+          </h5>
+        </div>
+        <div class="weather_details_box">
+          <img src="/img/visibility.png" alt="fog icon" />
+          <h4>Visibility</h4>
+          <h5>{{ todayWeather?.visibility ?? "N/A" }} meters</h5>
+        </div>
+        <div class="weather_details_box">
+          <img src="/img/humidity.png" alt="humidity icon" />
+          <h4>Humidity</h4>
+          <h5>{{ todayWeather?.main?.humidity ?? "N/A" }} %</h5>
+        </div>
+        <div class="weather_details_box">
+          <img src="/img/pressure.png" alt="pressure meter icon" />
+          <h4>Pressure</h4>
+          <h5>{{ todayWeather?.main?.pressure ?? "N/A" }} hPa</h5>
+        </div>
+        <div class="weather_details_box">
+          <img src="/img/wind.png" alt="wind meter icon" />
+          <h4>Wind</h4>
+          <h5>{{ Math.trunc((todayWeather?.wind?.speed ?? 0) * 3.6) }} km/h</h5>
+        </div>
+      </section>
 
-      <div>
-        <h4>Next Solar Eclipse:</h4>
-        <p>
-          {{ moon.sun?.next_solar_eclipse?.datestamp ?? "N/A" }} 🌑({{
-            moon.sun?.next_solar_eclipse?.type ?? "N/A"
-          }})
-        </p>
-      </div>
-      <div>
-        <h4>Next Lunar Eclipse:</h4>
-        <p>{{ moon.moon?.next_lunar_eclipse?.datestamp ?? "N/A" }}🌔</p>
-      </div>
-      <div>
-        <h4>Next Full Moon:</h4>
-        <p>
-          {{ moon?.value?.moon_phases?.full_moon?.next?.datestamp ?? "N/A" }}
-        </p>
-      </div>
-      <div>
-        <h4>Zodiac Sign:</h4>
-        <p>
-          🌞 Sun in {{ moon.moon?.zodiac?.sun_sign ?? "N/A" }} | 🌙 Moon in
-          {{ moon.moon?.zodiac?.moon_sign ?? "N/A" }}
-        </p>
-      </div>
-    </aside>
+      <aside v-if="moon" class="weather_moon">
+        <div class="weather_moon_box">
+          <h4>Sunrise & Sunset:</h4>
+          <p>
+            {{ moon.sun?.sunrise_timestamp ?? "N/A" }}🌅-{{
+              moon.sun?.sunset_timestamp ?? "N/A"
+            }}🌇
+          </p>
+        </div>
+        <div class="weather_moon_box">
+          <h4>Moonrise & Moonset:</h4>
+          <p>
+            {{ moon.moon?.moonrise ?? "N/A" }}🌙-{{
+              moon.moon?.moonset ?? "N/A"
+            }}🌓
+          </p>
+        </div>
+        <div class="weather_moon_box">
+          <h4>Current Moon Phase:</h4>
+          <p>
+            {{ moon.moon?.phase_name ?? "N/A" }}{{ moon.moon?.emoji ?? "" }}
+          </p>
+        </div>
+
+        <div class="weather_moon_box">
+          <h4>Next Solar Eclipse:</h4>
+          <p>
+            {{ moon.sun?.next_solar_eclipse?.datestamp ?? "N/A" }} 🌑({{
+              moon.sun?.next_solar_eclipse?.type ?? "N/A"
+            }})
+          </p>
+        </div>
+        <div class="weather_moon_box">
+          <h4>Next Lunar Eclipse:</h4>
+          <p>{{ moon.moon?.next_lunar_eclipse?.datestamp ?? "N/A" }}🌔</p>
+        </div>
+
+        <div class="weather_moon_box">
+          <h4>Zodiac Sign:</h4>
+          <p>
+            🌞 Sun in {{ moon.moon?.zodiac?.sun_sign ?? "N/A" }} | 🌙 Moon in
+            {{ moon.moon?.zodiac?.moon_sign ?? "N/A" }}
+          </p>
+        </div>
+      </aside>
+    </div>
   </div>
 </template>
-
-<style>
-.test {
-  border: 2px solid yellow;
-}
-</style>
